71b7269b78e61f645b2a5967e515b0837ef13026
Nam Phạm <36730406+SnightCoder@users.noreply.github.com>
2019-10-02 13:35:58 +0700

    Add files via upload

---
+++ OutlineSelectionResponse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(SelectionManager))]
+public class OutlineSelectionResponse : MonoBehaviour, ISelectionResponse
+{
+    public void OnDeselect(Transform selection)
+    {
+        var outline = selection.GetComponent<Outline>();
+        if (outline != null){
+            outline.OutlineWidth = 0;
+            outline.OutlineColor=Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        }
+    }
+
+    public void OnSelect(Transform selection)
+    {
+        var outline = selection.GetComponent<Outline>();
+        if (outline != null){
+            outline.OutlineWidth = 10;
+            outline.OutlineColor=Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        }
+    }
+
+}
---
+++ SelectionManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] private string selectableTag = "Selectable";
+    private ISelectionResponse _selectionResponse;
+    private Transform _selection;
+    void Awake()
+    {
+        _selectionResponse=GetComponent<ISelectionResponse>();
+    }
+    void Update()
+    {
+        // Deselection/Selection Response
+        if (_selection != null)
+        {
+            _selectionResponse.OnDeselect(_selection);
+        }
+
+        #region MyRegion
+        // Creating a Ray
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Selection Determination
+        _selection = null;
+        if (Physics.Raycast(ray, out var hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+
+                _selection = selection;
+            }
+        }
+        #endregion
+
+        // Deselection/Selection Response
+        if (_selection != null)
+        {
+            _selectionResponse.OnSelect(_selection);
+        }
+    }
+}
+
