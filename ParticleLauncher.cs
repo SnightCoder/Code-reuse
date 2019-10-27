d6191e1bb83576523585cae2f838a48a1160abe6
Nam Phạm <36730406+SnightCoder@users.noreply.github.com>
2019-10-27 20:27:15 +0700

    Add files via upload

---
+++ ParticleLauncher.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleLauncher : MonoBehaviour
+{
+    public ParticleSystem particleLauncher;
+    public ParticleSystem splatterParticles;
+    public List<ParticleCollisionEvent> collisionEvent;
+    public Gradient particleColorGradient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collisionEvent=new List<ParticleCollisionEvent>();
+    }
+
+    private void OnParticleCollision(GameObject other) {
+        ParticlePhysicsExtensions.GetCollisionEvents(particleLauncher,other,collisionEvent);
+        for(int i=0;i<collisionEvent.Count;i++){
+          EmitAtLocation(collisionEvent[i]);
+        }
+    }
+
+    void EmitAtLocation(ParticleCollisionEvent particleCollisionEvent){
+        splatterParticles.transform.position=particleCollisionEvent.intersection;
+        splatterParticles.transform.rotation=Quaternion.LookRotation(particleCollisionEvent.normal);
+
+            //ParticleSystem.MainModule   psMain=splatterParticles.main;
+           // psMain.startColor=particleColorGradient.Evaluate(Random.Range(0,1));
+           splatterParticles.startColor=particleColorGradient.Evaluate(Random.Range(0,1));
+
+        splatterParticles.Emit(1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1"))    {
+            ParticleSystem.MainModule   psMain=particleLauncher.main;
+            psMain.startColor=particleColorGradient.Evaluate(Random.Range(0,1));
+        particleLauncher.Emit(1);
+        }
+    }
+}
